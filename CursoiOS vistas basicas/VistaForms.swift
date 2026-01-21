370fa0215dc852a400639e64790b44788e1e495b
Eduardo <eduoie@gmail.com>
2026-01-21 14:50:03 +0100

    Vistas de formularios, inputs y toggles

---
+++ CursoiOS vistas basicas/VistaForms.swift
@@ -0,0 +1,18 @@
+//
+//  VistaForms.swift
+//  CursoiOS vistas basicas
+//
+//  Created by Equipo 2 on 21/1/26.
+//
+
+import SwiftUI
+
+struct VistaForms: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaForms()
+}
---
+++ CursoiOS vistas basicas/VistaTogglesSliders.swift
@@ -0,0 +1,18 @@
+//
+//  VistaTogglesSliders.swift
+//  CursoiOS vistas basicas
+//
+//  Created by Equipo 2 on 21/1/26.
+//
+
+import SwiftUI
+
+struct VistaTogglesSliders: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaTogglesSliders()
+}
