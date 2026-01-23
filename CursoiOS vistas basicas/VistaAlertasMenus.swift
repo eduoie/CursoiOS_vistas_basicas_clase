2eab4f9b5e00851df234053882982e831ed63afe
Eduardo <eduoie@gmail.com>
2026-01-23 09:49:30 +0100

    Vistas para Touch Gestures, alertas y menús

---
+++ CursoiOS vistas basicas/VistaAlertasMenus.swift
@@ -0,0 +1,18 @@
+//
+//  VistaAlertasMenus.swift
+//  CursoiOS vistas basicas
+//
+//  Created by Equipo 2 on 22/1/26.
+//
+
+import SwiftUI
+
+struct VistaAlertasMenus: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaAlertasMenus()
+}
---
+++ CursoiOS vistas basicas/VistaTouchGestures.swift
@@ -0,0 +1,18 @@
+//
+//  VistaTouchGestures.swift
+//  CursoiOS vistas basicas
+//
+//  Created by Equipo 2 on 22/1/26.
+//
+
+import SwiftUI
+
+struct VistaTouchGestures: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaTouchGestures()
+}
